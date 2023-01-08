d861d4f2f5a1906a17188735df625fa9c41398aa
Josh Goldberg <git@joshuakgoldberg.com>
2023-01-08 14:54:22 -0500

    Added back some missing files, and deleted new ones

--- src/lib/markdownToHtml.ts
+++
@@ -1,8 +0,0 @@
-import { compile } from "@mdx-js/mdx";
-import remarkGfm from "remark-gfm";
-
-const remarkPlugins = [remarkGfm];
-
-export default async function markdownToHtml(markdown: string) {
-	return (await compile(markdown, { remarkPlugins })).toString();
-}
--- src/lib/types.ts
+++
@@ -1,5 +0,0 @@
-export type ReturnedProps<Getter extends (...args: never) => unknown> = Awaited<
-	ReturnType<Getter>
-> extends { props: infer Props }
-	? Props
-	: never;
